6143b4ebc70f865515c864fb0b2a2a2b2ff0959f
mininuke69 <118979770+mininuke69@users.noreply.github.com>
2023-03-05 14:16:44 +0100

    update swap.py

--- swap.py
+++ swap.py
@@ -1,13 +1,43 @@
 import requests as rq
+from json import load
 
 
-endpoint = 'https://api.coingecko.com/api/v3'
+gecko_endpoint = 'https://api.coingecko.com/api/v3'
+ban_node_endpoint = 'https://kaliumapi.appditto.com/api'
+xno_node_endpoint = None
+
+wallets = open('accounts.txt', 'r').read()
+ban_seed = wallets['ban']['seed']
+ban_address = wallets['ban']['address']
+xno_seed = wallets['xno']['seed']
+xno_address = wallets['xno']['adress']
+
+bal_ban = None
+bal_xno = None
+
+
+def Update_Balance():
+    global bal_ban
+    global bal_xno
+
+
+    ban_json = {
+                "action": "account_balance",
+                "account": ban_address,
+                }
+    bal_ban_request = rq.post(ban_node_endpoint, json=ban_json).json()
+    bal_ban = bal_ban_request['balance_decimal']
+
+
+
+
+
 
 print('connecting to CoinGecko...')
-ping = rq.get(f'{endpoint}/ping').json()['gecko_says']
+ping = rq.get(f'{gecko_endpoint}/ping').json()['gecko_says']
 print(f'connected, gecko says: {ping}')
 
-#price_list = rq.get(f'{endpoint}/simple/price?ids=banano%2Cnano&vs_currencies=usd&precision=18').json()
+#price_list = rq.get(f'{gecko_endpoint}/simple/price?ids=banano%2Cnano&vs_currencies=usd&precision=18').json()
 price_list = {'banano': {'usd': 0.004928284617909525}, 'nano': {'usd': 0.8635332214522783}} #temporary solution to reduce api calls
 
 
@@ -16,3 +46,6 @@ price_ban_usd = price_list['banano']['usd']
 
 ban_to_xno = price_ban_usd / price_xno_usd
 xno_to_ban = price_xno_usd / price_ban_usd
+
+Update_Balance()
+print(bal_ban)
