db6861cffc9d9ed9a503afb9a336dc7c395ff8d0
mininuke69 <118979770+mininuke69@users.noreply.github.com>
2023-03-05 10:10:13 +0100

    price calculations

--- swap.py
+++ swap.py
@@ -7,13 +7,12 @@ print('connecting to CoinGecko...')
 ping = rq.get(f'{endpoint}/ping').json()['gecko_says']
 print(f'connected, gecko says: {ping}')
 
-price_list = rq.get(f'{endpoint}/simple/price?ids=banano%2Cnano&vs_currencies=sat%2C&precision=18').json()
+#price_list = rq.get(f'{endpoint}/simple/price?ids=banano%2Cnano&vs_currencies=usd&precision=18').json()
+price_list = {'banano': {'usd': 0.004928284617909525}, 'nano': {'usd': 0.8635332214522783}} #temporary solution to reduce api calls
 
-print(price_list)
 
 price_xno_usd = price_list['nano']['usd']
-price_xno_sat = price_list['nano']['sat']
 price_ban_usd = price_list['banano']['usd']
-price_ban_sat = price_list['banano']['sat']
 
-
+ban_to_xno = price_ban_usd / price_xno_usd
+xno_to_ban = price_xno_usd / price_ban_usd
